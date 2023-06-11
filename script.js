0afaa9a234fc238f3dbda561c326fe200b5dad4e
Pragati Srivastava <102436055+prag-sri@users.noreply.github.com>
2023-06-11 23:04:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,40 +5,68 @@ let arr = [
     { id: 2, name: "jack", age: "20", marks: 85 },
     { id: 3, name: "karen", age: "19", marks: 35 },
   ];
-  
+
   function PrintStudentswithMap() {
     //Write your code here , just console.log
-    const students = arr.filter((student) => student.marks > 50);
-    const studentNames = students.map((student) => student.name);
-    console.log(studentNames);
+    let modifiedArr= arr.map((obj) => {
+        if(obj.marks>50)
+            return obj;
+    });
+    console.log(modifiedArr);
   }
-  
+
   function PrintStudentsbyForEach() {
     //Write your code here , just console.log
-    const students = arr.filter((student) => student.marks > 50);
-    students.forEach((student) => console.log(student));
+    arr.forEach(function(obj){
+        if(obj.marks>50)
+            console.log(obj);
+    })
   }
-  
+
   function addData() {
     //Write your code here, just console.log
-    const newStudent = { id: 4, name: "susan", age: "20", marks: 45 };
-    arr.push(newStudent);
+    let newObj= {
+        id: 4, 
+        name: "susan", 
+        age: "20", 
+        marks: 45  
+    }
+    arr.push(newObj);
     console.log(arr);
   }
-  
+
   function removeFailedStudent() {
     //Write your code here, just console.log
-    arr = arr.filter((student) => student.marks >= 50);
+    arr.filter((obj, index) => {
+        if(obj.marks<=50)
+            delete arr[index];
+    })
     console.log(arr);
   }
-  
+
   function concatenateArray() {
     //Write your code here, just console.log
-    const newArray = [
-        { id: 4, name: "lisa", age: "18", marks: 90 },
-        { id: 5, name: "peter", age: "19", marks: 65 },
-        { id: 6, name: "emily", age: "20", marks: 75 },
-      ];
-      arr = arr.concat(newArray);
-      console.log(arr);
+    let newObj1= {
+        id: 4, 
+        name: "priyam", 
+        age: "21", 
+        marks: 48  
+    }
+    let newObj2= {
+        id: 5, 
+        name: "shivam", 
+        age: "18", 
+        marks: 66  
+    }
+    let newObj3= {
+        id: 6, 
+        name: "radhika", 
+        age: "23", 
+        marks: 82 
+    }
+    arr.push(newObj1);
+    arr.push(newObj2);
+    arr.push(newObj3);
+    console.log(arr);
   }
+concatenateArray();
